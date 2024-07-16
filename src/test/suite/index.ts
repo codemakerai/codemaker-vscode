ef11361789d6ded21332f99828e8dd6cb1735a22
codemakerai-dev <codemaker-dev@codemaker.ai>
2024-07-16 15:04:42 -0700

    Updated package dependencies

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -12,11 +12,7 @@ export function run(): Promise<void> {
 	const testsRoot = path.resolve(__dirname, '..');
 
 	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
+		glob('**/**.test.js', { cwd: testsRoot }).then((files) => {
 			// Add files to the test suite
 			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
@@ -33,6 +29,6 @@ export function run(): Promise<void> {
 				console.error(err);
 				e(err);
 			}
-		});
+		}).catch(err => e(err));
 	});
 }
